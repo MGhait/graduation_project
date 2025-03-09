7b7693c8da535d8cb42b3124ed140b773ee62281
Mohamed Saad <mosaad05019@gmail.com>
2025-03-10 00:00:53 +0200

    intial-policy-pages

---
+++ resources/views/dataDeletion.blade.php
@@ -0,0 +1,27 @@
+<div>
+    If you would like to delete your data from our system, please follow the steps below:
+
+    Send a Request:
+
+    Email us at support@gadetguru.mgheit.com with the subject line: "Request for Data Deletion".
+
+    Include the following information in your email:
+
+    Your full name.
+
+    The email address associated with your account.
+
+    A description of the data you would like to delete.
+
+    Verification:
+
+    We will verify your identity to ensure the request is legitimate.
+
+    Once verified, we will process your request within 30 days.
+
+    Confirmation:
+
+    After your data has been deleted, we will send you a confirmation email.
+
+    If you have any questions, please contact us at support@gadetguru.mgheit.com.
+</div>
