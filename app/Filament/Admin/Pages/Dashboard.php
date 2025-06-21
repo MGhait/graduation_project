a42a7b51baa367801d423f360e9c9a90083d7ead
Mohamed Saad <mosaad05019@gmail.com>
2025-06-21 19:46:21 +0300

    fix grid

--- app/Filament/Admin/Pages/Dashboard.php
+++ app/Filament/Admin/Pages/Dashboard.php
@@ -2,6 +2,9 @@
 
 namespace App\Filament\Admin\Pages;
 
+use App\Filament\Admin\Resources\ICResource\Widgets\TopViewedICs;
+use App\Filament\Admin\Resources\UserResource\Widgets\TotalNumber;
+use App\Filament\Admin\Resources\UserResource\Widgets\UsersChart;
 use Filament\Pages\Page;
 
 class Dashboard extends Page
@@ -9,4 +12,26 @@ class Dashboard extends Page
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.admin.pages.dashboard';
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            TotalNumber::class,
+            TopViewedICs::class,
+            UsersChart::class,
+        ];
+    }
+
+    public function getHeaderWidgetsColumns(): int|array|string
+    {
+        return 12;
+    }
+    protected function getFooterWidgets(): array
+    {
+        return [
+
+
+        ];
+    }
+
 }
