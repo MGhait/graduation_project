f4b1c70a871ad68d9e201ba42e921be4efbc3fb8
Mohamed Saad <mosaad05019@gmail.com>
2025-06-25 19:07:48 +0300

    update user widgets

--- app/Filament/Admin/Resources/UserResource/Widgets/TotalNumber.php
+++ app/Filament/Admin/Resources/UserResource/Widgets/TotalNumber.php
@@ -3,18 +3,18 @@
 namespace App\Filament\Admin\Resources\UserResource\Widgets;
 
 use App\Models\IC;
-use App\Models\Store;
+use App\Models\Message;
 use App\Models\User;
 use EightyNine\FilamentAdvancedWidget\AdvancedStatsOverviewWidget as BaseWidget;
 use EightyNine\FilamentAdvancedWidget\AdvancedStatsOverviewWidget\Stat;
 
 class TotalNumber extends BaseWidget
 {
+    protected int|string|array $columnSpan = 'full';
     protected function getStats(): array
     {
-        $lastMonth = now()->subMonth(2);
         return [
-            Stat::make("Total Users",User::count())
+            Stat::make("Total Users", User::count())
                 ->icon('heroicon-s-users')
                 ->progress(69)
                 ->progressBarColor(50)
@@ -22,16 +22,25 @@ class TotalNumber extends BaseWidget
                 ->description('The users in this period')
                 ->descriptionIcon('heroicon-o-chevron-right', 'after')
                 ->iconColor('info'),
-            Stat::make("New users", User::where('created_at', '>=', $lastMonth)->count())
-                ->icon('heroicon-s-user')
+            Stat::make("Saved ICs", IC::sum('likes'))
+                ->icon('heroicon-s-bookmark')
                 ->progressBarColor(50)
                 ->iconColor('secondary')
                 ->iconPosition('start')
                 ->description('User Joined Us last 30 days')
                 ->descriptionIcon('heroicon-o-chevron-down', 'after')
                 ->progress(20),
-            Stat::make("Total ICs", IC::count())
-                ->icon('heroicon-s-cpu-chip')
+//            Stat::make("New users", User::where('created_at', '>=', $lastMonth)->count())
+//                ->icon('heroicon-s-user')
+//                ->progressBarColor(50)
+//                ->iconColor('secondary')
+//                ->iconPosition('start')
+//                ->description('User Joined Us last 30 days')
+//                ->descriptionIcon('heroicon-o-chevron-down', 'after')
+//                ->progress(20),
+
+            Stat::make("New Messages", Message::where('status', 'unread')->count())
+                ->icon('heroicon-s-envelope')
                 ->progressBarColor(50)
                 ->iconPosition('start')
                 ->description('The ICs in our Database')
@@ -39,14 +48,6 @@ class TotalNumber extends BaseWidget
                 ->progressBarColor(50)
                 ->iconColor('warning')
                 ->chartColor('danger')
-                ->progress(80),
-            Stat::make("Total Stores", Store::count()+1)
-                ->icon('heroicon-s-eye')
-                ->progressBarColor(50)
-                ->iconPosition('start')
-                ->description('Total Stores Worked With Us')
-                ->descriptionIcon('heroicon-o-chevron-up', 'after')
-                ->iconColor('light') // can be added form the AdminPanelProvider
                 ->progress(80),
         ];
     }
