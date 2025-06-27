36d9243d39ba7b293384cc0d055251f0f4504433
Mohamed Saad <mosaad05019@gmail.com>
2025-06-27 20:22:54 +0300

    OAuth module

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ApiResponse;
+use App\Helpers\Username;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
@@ -18,40 +19,40 @@ class SocialiteController extends Controller
     {
         try {
             $socialUser = Socialite::driver($provider)->user();
-            $user = User::where('email', $socialUser->getEmail())->first(); // do i really need this ?
-            if ($provider == 'facebook') {
-                $firstName = $socialUser->user['first_name'] ?? $socialUser->getName(); // need to be divided
-                $lastName = $socialUser->user['last_name'] ?? '';
-            }
-            elseif ($provider == 'google') {
-                $firstName = $socialUser->user['given_name'] ?? $socialUser->getName();
-                $lastName = $socialUser->user['family_name'] ?? '';
-            }
-            else {
-                $firstName = $socialUser->getName();
-                $lastName =  ' ';
+            $user = User::where('email', $socialUser->getEmail())->first();
+            $nameParts = explode(' ', $socialUser->getName() ?? '');
+            $firstName = $nameParts[0] ?? '';
+            $lastName = $nameParts[1] ?? '';
+
+            if ($provider === 'facebook' || $provider === 'google') {
+                $firstName = $socialUser->user['first_name'] ?? $socialUser->user['given_name'] ?? $firstName;
+                $lastName = $socialUser->user['last_name'] ?? $socialUser->user['family_name'] ?? $lastName;
             }
             if (!$user) {
-                $newUser = User::updateOrCreate([
-                    'provider' => $provider,
-                    'provider_id' => $socialUser->getId(),
-                ],[
+                $user = User::create([
                     'first_name' => $firstName,
                     'last_name' => $lastName,
+                    'username' => Username::generateUsername($firstName, $lastName),
                     'email' => $socialUser->getEmail(),
                     'provider' => $provider,
                     'provider_id' => $socialUser->getId(),
                     'email_verified_at' => now(),
                 ]);
+            } else {
+                $user->update([
+                    'provider' => $provider,
+                    'provider_id' => $socialUser->getId(),
+                    'email_verified_at' => $user->email_verified_at ?? now(),
+                ]);
             }
-            $token = $newUser->createToken('APIToken')->plainTextToken;
+            $token = $user->createToken('APIToken')->plainTextToken;
             $data = [
                 'token' => $token,
                 'first_name' => $firstName,
                 'last_name' => $lastName,
-                'email' => $newUser->email,
+                'email' => $user->email,
             ];
-            return ApiResponse::sendResponse(200,'User Logged In Successfully',$data);
+            return ApiResponse::sendResponse(200,'User Logged In via ' . $provider .' Successfully',$data);
         }
         catch (\Exception $e) {
             return ApiResponse::sendResponse(500,'Something Went Wrong',['error'=>$e->getMessage()]);
